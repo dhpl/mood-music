75bf30857e4880896cee001e35ec1d867a4d724d
longdhp <longdhp@di4l.vn>
2021-07-16 18:33:03 +0700

    fix: drop table sings

---
+++ database/migrations/2021_07_16_113233_drop_table_sings_4.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DropTableSings4 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sings', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('sings', function (Blueprint $table) {
+            $table->dropForeign('sings_playlist_id_foreign');
+            $table->dropColumn('playlist_id');
+            $table->dropIfExists('sings');
+        });
+    }
+}
